c93a5f3a6c2af7cb21221550857e0983e34b268a
Cristian Machado <cristian.machado@correounivalle.edu.co>
2025-02-24 11:45:08 -0500

    loading

--- dist/assets/404-DI-DeTvZ.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Cvx6UvUP.js";function t(){return o.jsx("h1",{children:"Hola"})}export{t as default};
--- src/Components/Loarder/Loarder.tsx
+++ src/Components/Loarder/Loarder.tsx
@@ -3,8 +3,8 @@ import Box from '@mui/material/Box';
 
 export default function Loader() {
     return (
-      <Box sx={{ textAlign: "center", marginTop: "50px" }}>
-        <h2>Cargando...</h2>
+      <Box sx={{ textAlign: "center", marginTop: "50px", background: '#222', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h2 clasName="loader"/>
       </Box>
     );
 }
